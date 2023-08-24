d35758f9ccfaac28989d560b9ab2373daf5ca415
Shathiso <shathisontibi@yahoo.com>
2023-08-24 11:48:08 +0200

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import typography from '@tailwindcss/typography';
 import forms from '@tailwindcss/forms';
+import aspectRatio from '@tailwindcss/aspect-ratio';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,7 +9,24 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/views/livewire/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './vendor/wire-elements/modal/config/livewire-ui-modal.php',
+        './vendor/wire-elements/modal/**/*.blade.php',
     ],
+    purge: {
+        content: [
+          './vendor/wire-elements/modal/resources/views/*.blade.php',
+          './storage/framework/views/*.php',
+          './resources/views/**/*.blade.php',
+        ],
+        options: {
+          safelist: [
+            'sm:max-w-2xl'
+          ]
+        }
+    },
 
     theme: {
         extend: {
@@ -17,5 +36,9 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        typography,
+        aspectRatio
+    ],
 };
